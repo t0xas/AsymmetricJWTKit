d9637a0c45ff58f578b0eeae677c7cd9d7ad2460
Anton Zabolotnikob <azabolotnikov@ronasit.com>
2025-08-30 10:11:18 +0500

    chore: configure gitingore

--- tests/IssuerTest.php
+++ tests/IssuerTest.php
@@ -12,8 +12,8 @@ class IssuerTest extends TestCase
     public function testEncode()
     {
         $config = new Config(
-            pathRootSer: __DIR__.DIRECTORY_SEPARATOR.'tests'. DIRECTORY_SEPARATOR . 'fixtures',
-            pathToCASer: __DIR__.DIRECTORY_SEPARATOR.'tests'. DIRECTORY_SEPARATOR . 'fixtures',
+            pathRootSer: __DIR__ . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . 'fixtures',
+            pathToCASer: __DIR__ . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . 'fixtures',
             owner: new CertificateOwnerDTO(
                 countryName: 'US',
                 stateOrProvinceName: 'CA',
@@ -26,5 +26,7 @@ class IssuerTest extends TestCase
         );
 
         $token = (new Issuer($config))->createToken();
+
+
     }
 }
